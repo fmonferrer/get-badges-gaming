819043b441b38a2480c317bef030ce9b879a3944
fernando.monferrer <fernando.monferrer@mango.com>
2018-07-18 00:01:54 +0200

    Add test ok

---
+++ src/test/java/HelloShould.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HelloShould {
+
+    @Test
+    public void sayHelloWorld(){
+        assertTrue(true);
+    }
+}
